d7297f7cd885e10f71d2972aa343f013b181b290
modengann <99417989+modengann@users.noreply.github.com>
2022-08-29 11:49:29 -0400

    Update transform.py

--- src/transform.py
+++ src/transform.py
@@ -1,23 +1,13 @@
 #!/usr/bin/env python3
 
 def transform(s1, s2):
-    s1_modify = split_return(s1)
-    s2_modify = split_return(s2)
-    temp = list(zip(s1_modify,s2_modify))
-    
-    temp = list(map(lambda L: L[0]*L[1], temp))
-
-    return temp
+    pass
 
 def main():
-    print(transform("1 5 3", "2 6 -1"))
+    pass
     
 def split_return(l):
-    temp_list = l.split()
-    temp = []
-    for i in temp_list:
-        temp.append(int(i))
-    return temp
+    pass
 
 if __name__ == "__main__":
     main()
